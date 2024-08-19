b155297692ed8633e7df842b617d0920eb66ebea
gyuri <rbfl7645@naver.com>
2024-08-19 21:13:27 +0900

    모달

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <template>
+
   <div>
+    <div class="black-bg" v-if="modalOpen == true">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>상세페이지임</p>
+        <button @click="closeModal">닫기</button>
+      </div>  
+    </div>
+
     <div class="menu">
       <a v-for="(nav,i) in menuItems" :key="i">{{nav}}</a>
     </div>
-    
-    <h1>원룸샵</h1>
-    
+
     <div class="list" v-for="(list, i) in products" :key="i">
-      <h4>{{list}}</h4>
+      <img src="./assets/room0.jpg" class="room-img"/>
+      <h4 @click="openModal">{{list}}</h4>
       <p>60 만원</p>
+      <button @click="increase(i)">허위매물 신고</button> <span>신고 수 : {{reportCnt[i]}}</span>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data(){
-    return{
-      menuItems : ['Home', 'Shop', 'About'],
-      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
-    }
-  }, components:{
+  export default {
+    name: 'App',
+    data(){
+      return{
+        menuItems : ['Home', 'Shop', 'About'],
+        products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+        reportCnt : [0, 0, 0],
+        modalOpen : false,
+      }
 
-  },
-}
-</script>
+    }, methods : {
+      increase(idx){
+        this.reportCnt[idx]++;
+      }, openModal(){
+         this.modalOpen = true; 
+      }, closeModal(){
+         this.modalOpen = false;
+      }
+
+    }, components:{
+
+    },
+  }
+  </script>
 
 <style>
 #app {
@@ -34,6 +55,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+body {
+  margin : 0;
 }
 
 .menu{
@@ -44,5 +68,23 @@ export default {
 .menu a {
   color : white;
   padding : 10px;
+}
+.room-img{
+  width  : 30%;
+  margin-top : 40px;
+}
+div {
+  box-sizing : border-box;
+}
+.black-bg{
+  width : 100%; height: 100%;
+  background : rgba(0, 0, 0, 0.5);
+  position : fixed; padding :20px;
+}
+.white-bg{
+  width : 100%;
+  background :white;
+  border-radius : 8px;
+  padding :20px;
 }
 </style>
