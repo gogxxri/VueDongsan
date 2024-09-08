4150dfe79d3f8cf44e5306b922dc460fe48e083d
gyuri <rbfl7645@naver.com>
2024-09-08 23:55:46 +0900

    사용자 input 받기

--- src/components/DetailModal.vue
+++ src/components/DetailModal.vue
@@ -4,7 +4,15 @@
       <img :src="onerooms[selectedID].image" />
       <h4>{{ onerooms[selectedID].title }}</h4>
       <h6>{{ onerooms[selectedID].content }}</h6>
-      <p>{{ onerooms[selectedID].price }} 원</p>
+      <p>{{ onerooms[selectedID].price}} 원</p>
+      <!-- <input @input="month=$event.target.value">개월 -->
+      <input v-model.number="month"/>개월 <!--v-model input에 입력한 데이터 state에 저장-->
+      <!-- <select v-model="month">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>개월 -->
+      <p>{{month}}개월 : {{ onerooms[selectedID].price * month }} 원</p>
       <DiscountBanner />
       <button @click="closeModal">닫기</button>
     </div>
@@ -16,6 +24,11 @@ import DiscountBanner from './Discount.vue';
 
 export default {
   name: 'DetailModal',
+  data(){
+    return {
+      month : 1,
+    }
+  }, 
   props: {
     onerooms: Array,
     selectedID: Number,
--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -3,7 +3,7 @@
         <img :src="room.image" class="room-img" />
         <h4 @click="$emit('openModal', room.id)">{{ room.title }}</h4>
         <p>{{ room.price }} 원</p>
-        <button @click.stop="increase(room.id)">허위매물 신고</button> 
+        <button @click="increase(room.id)">허위매물 신고</button> 
         <span>신고 수: {{ room.reportCnt }}</span> 
     </div>
 </template>
