33de1fee9e4ca62871a8bd844226d75a8175de0f
gyuri <rbfl7645@naver.com>
2024-09-11 19:05:50 +0900

    ajax 요청하기

---
+++ vuestargram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Post :posting="posting" v-for="(posting, i) in posting" :key="i"/>
+    </div>
+</template>
+
+<script>
+import Post from './Post';
+export default {
+    name : 'ContainerComponent',
+    props : {
+        posting : Array,
+    },
+    components : {
+        Post,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuestargram/src/components/Post.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="post"> 
+        <div class="post-header">
+        <div class="profile" :style="{ backgroundImage: `url(${posting.userImage})` }"></div>
+        <span class="profile-name">{{posting.name}}</span>
+        </div>
+        <div class="post-body" :style="{ backgroundImage: `url(${posting.postImage})` }"></div>
+        <div class="post-content">
+        <p>❤ {{posting.likes}}</p>
+        <p><strong>{{posting.name}}</strong> {{posting.content}}</p>
+        <p class="date">{{posting.date}}</p>
+        </div>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'postComponent',
+    props : {
+        posting : Array,
+    },
+}
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+} 
+</style>
